2d0b4defab6702a981a9d740d1ec6b1d8433ab1a
paveltur <haePH9J75NWgv@e>
2022-11-01 01:43:00 +0300

    old_copying + s23_copy_all_rows

--- main.py
+++ main.py
@@ -3,40 +3,41 @@ import datetime as dt
 import smtplib
 import json
 
+column_a = "A2:A"
+columns_ad = "A2:D"
+columns_cd = "C2:D"
+column_m = "M2:M"
+date_column = "G2:G"
+range_for_copy = "A2:U"
+DAYS = 5
+
 with open("creds_sheets.json", "r") as f:
     creds_sheets = json.load(fp=f)
-
-phone_calls = creds_sheets["phone_calls"]
-
+CALLS = creds_sheets["phone_calls"]
 HISTORY = creds_sheets["HISTORY"]
-
-column_a = "A2:A"
-column_cd = "C2:D"
-column_m = "M2:M"
-columns_for_date = ["Q4_22!G2:G"]
-range_for_copy = "A2:U"
 MY_EMAIL = creds_sheets["mail_creds"]["MY_EMAIL"]
 PASSWORD = creds_sheets["mail_creds"]["PASSWORD"]
 TO_EMAILS = creds_sheets["mail_creds"]["TO_EMAILS"]
 
 
-def copying_main_data(name,
-                      call_list,
-                      range_cd=column_cd,
-                      range_a=column_a,
-                      range_copy=range_for_copy,
-                      history_data=HISTORY["history_data"],
-                      history_sheet_id=HISTORY["history_sheet_id"],
-                      history_range_read=HISTORY["history_range_read"]):
-
-    num_rows = len(ApiGoogle(call_list, f"{name}!{range_cd}").read_data_ranges()["valueRanges"][0]["values"])
+def copying_main_data(name: str,
+                      call_list: str,
+                      history_data: str,
+                      history_sheet_id: str,
+                      history_sheet_name: str):
+    num_rows = len(ApiGoogle(call_list, f"{name}!{columns_cd}").read_data_ranges()["valueRanges"][0]["values"])
+    # получаем количество строк на листе обзвона
     if name == "pdl_old" or name == "il":
-        ApiGoogle(call_list, f"{name}!{range_a}").update_data({"values": [[f'{name}']]*num_rows})
+        ApiGoogle(call_list, f"{name}!{column_a}").update_data({"values": [[f'{name}']]*num_rows})
+    # для илов и пдл_олд добавляем имя обзвона в столбец А
 
-    data_1 = ApiGoogle(history_data, history_range_read).read_data_ranges()
-    data_2 = ApiGoogle(call_list, [f"{name}!{range_copy}{num_rows+1}"]).read_data_ranges()
+    data_1 = ApiGoogle(history_data, f"{history_sheet_name}!{columns_ad}").read_data_ranges()
 
-    try:
+    data_2 = ApiGoogle(call_list, [f"{name}!{range_for_copy}{num_rows + 1}"]).read_data_ranges()
+    # забираем весь диапазон листа обзвона для копирования (прим. листы периодически очищаются для быстроты загрузки,
+    # поэтому забираем диапазон полностью)
+
+    try:  # делаем список для поиска последнего исторического значения
         list_for_last_hist_values = data_1["valueRanges"][0]["values"]
     except KeyError:
         list_for_last_hist_values = []
@@ -50,24 +51,34 @@ def copying_main_data(name,
                     list_of_rows.append(row)
             except IndexError:
                 continue
-
         try:
             last_hist_value = [el for el in list_for_last_hist_values if el[0] == "step 7" or el[0] == "decision"][-1]
         except IndexError:
             last_hist_value = None
+
     elif name == "step 2-3":
         check_column = 2
         list_of_rows = []
-        for row in data_2["valueRanges"][0]["values"]:
-            try:
-                if row[0] == "step 2-3" and row[12]:
-                    list_of_rows.append(row)
-            except IndexError:
-                continue
+        if history_sheet_name == "Q4_22":
+            for row in data_2["valueRanges"][0]["values"]:
+                try:
+                    if row[0] == "step 2-3" and row[12]:
+                        list_of_rows.append(row)
+                except IndexError:
+                    continue
+        else:
+            for row in data_2["valueRanges"][0]["values"]:
+                try:
+                    if row[0] == "step 2-3":
+                        list_of_rows.append(row)
+                except IndexError:
+                    continue
+
         try:
             last_hist_value = [el for el in list_for_last_hist_values if el[0] == f"{name}"][-1]
         except IndexError:
             last_hist_value = None
+
     else:
         check_column = 3
         list_of_rows = [row for row in data_2["valueRanges"][0]["values"] if row[0] == f"{name}"]
@@ -92,14 +103,12 @@ def copying_main_data(name,
         last_called_index = None
 
     range_body = {"values": list_of_rows[index_cut_list:last_called_index]}
-
     try:
-        last_row_history_list = len(data_1['valueRanges'][0]['values']) + 2
+        last_row_history_list = len(list_for_last_hist_values) + 2
     except KeyError:
         last_row_history_list = 2
 
-    range_for_writing = f"Q4_22!A{last_row_history_list}:U"
-
+    range_for_writing = f"{history_sheet_name}!A{last_row_history_list}:U"
     if not range_body["values"]:
         return {"name": name, "func": "copied", "result": "true_2", "info": "no data to copy"}
     else:
@@ -108,9 +117,10 @@ def copying_main_data(name,
         return {"name": name, "func": "copied", "result": "true", "info": add_new_data}
 
 
-def add_statuses_as_formula(data: dict, history_data=HISTORY["history_data"]):
-    nums_cells = []
+def add_statuses_as_formula(data: dict):
+    sheet_name = data["info"]["updatedRange"].split("!")[0]
     list_cells = data["info"]["updatedRange"].split("!")[1].split(":")
+    nums_cells = []
     for el in list_cells:
         new_el = "".join(char for char in el if char.isdigit())
         nums_cells.append(int(new_el))
@@ -125,28 +135,54 @@ def add_statuses_as_formula(data: dict, history_data=HISTORY["history_data"]):
             formulas_h_column.append([f'=ЕСЛИОШИБКА(ВПР(D{num_row};draft_auto!$E:$G;2;0);"")'])
             formulas_i_column.append([f'=ЕСЛИОШИБКА(ВПР(D{num_row};draft_auto!$E:$G;3;0);"")'])
 
-    info_for_writing = [{"range_for_writing": f"Q4_22!H{nums_cells[0]}:H{nums_cells[1]}",
+    info_for_writing = [{"range_for_writing": f"{sheet_name}!H{nums_cells[0]}:H{nums_cells[1]}",
                          "body": {"values": formulas_h_column}},
-                        {"range_for_writing": f"Q4_22!I{nums_cells[0]}:I{nums_cells[1]}",
+                        {"range_for_writing": f"{sheet_name}!I{nums_cells[0]}:I{nums_cells[1]}",
                          "body": {"values": formulas_i_column}}]
     result = []
     for column in info_for_writing:
-        statuses_results = ApiGoogle(history_data, column["range_for_writing"]).update_data(column["body"])
+        statuses_results = ApiGoogle(data["info"]["spreadsheetId"],
+                                     column["range_for_writing"]).update_data(column["body"])
         result.append({"name": data["name"], "func": "added",
                        "result": "true", "info": statuses_results})
     return result
 
 
-def backup_statuses(history_data=HISTORY["history_data"],
-                    history_sheet_id=HISTORY["history_sheet_id"],
-                    days_before_backup_statuses=5,
-                    start_row=None,
-                    end_row=None):
+def check_backup_statuses(history_data, history_sheet_name, start_row, end_row, date_backup_statuses):
+    data_stat_value = ApiGoogle(history_data, [f"{history_sheet_name}!A{start_row + 1}:I{end_row}"]).read_data_ranges()
+    calls_info = []
+    if history_sheet_name == "Q4_22":
+        list_of_name_phone_calls = [call["name"] for call in CALLS]
+        for name in list_of_name_phone_calls:
+            if name == "s7d":
+                rows = [row for row in data_stat_value["valueRanges"][0]["values"]
+                        if row[0] == "step 7" or row[0] == "decision"]
+            else:
+                rows = [row for row in data_stat_value["valueRanges"][0]["values"] if row[0] == name]
+            all_rows = len(rows)
+            statuses = len([el[0] for el in rows if len(el) > 7])
+            reason_code = len([el[0] for el in rows if len(el) > 8])
+            calls_info.append({f"{name}": {"all_rows": all_rows, "statuses": statuses, "reason_code": reason_code}})
+    else:
+        name = "step 2-3"
+        rows = [row for row in data_stat_value["valueRanges"][0]["values"] if row[0] == name]
+        all_rows = len(rows)
+        statuses = len([el[0] for el in rows if len(el) > 7])
+        reason_code = len([el[0] for el in rows if len(el) > 8])
+        calls_info.append({f"{name}": {"all_rows": all_rows, "statuses": statuses, "reason_code": reason_code}})
 
+    return {"name": "statuses + reson code", "func": "backed up", "result": "true",
+            "info": {"date_for_backup": date_backup_statuses, "detail_info": calls_info}}
+
+
+def backup_statuses(history_data,
+                    history_sheet_id,
+                    history_sheet_name,
+                    days_before_backup_statuses=DAYS):
     hours_for_backup = (days_before_backup_statuses * 24) + 12
     hours_for_end_row = ((days_before_backup_statuses - 1) * 24) + 12
     date_backup_statuses = (dt.datetime.now() - dt.timedelta(hours=hours_for_backup)).strftime("%Y-%m-%d")
-    data_3 = ApiGoogle(history_data, columns_for_date).read_data_ranges()
+    data_3 = ApiGoogle(history_data, [f"{history_sheet_name}!{date_column}"]).read_data_ranges()
     list_of_dates = []
     for row in data_3["valueRanges"][0]["values"]:
         if not row:
@@ -154,65 +190,50 @@ def backup_statuses(history_data=HISTORY["history_data"],
         else:
             list_of_dates.append(row[0].split(" ")[0])
 
+    start_row = None
     for row in enumerate(list_of_dates):
         if row[1] == date_backup_statuses:
             start_row = row[0] + 1
             break
 
+    end_row = None
     for row in enumerate(list_of_dates):
         if row[1] == (dt.datetime.now() - dt.timedelta(hours=hours_for_end_row)).strftime("%Y-%m-%d"):
             end_row = row[0] + 1
             break
 
     backup_old_statuses_h_i = {
-            "requests": [
-                {
-                    "copyPaste": {
-                        "source": {
-                            "sheetId": history_sheet_id,
-                            "startRowIndex": start_row,
-                            "endRowIndex": end_row,
-                            "startColumnIndex": 7,  # столбец Н, по умолчанию 7
-                            "endColumnIndex": 9  # столбец I, по умолчанию 9
-                        },
-                        "destination": {
-                            "sheetId": history_sheet_id,
-                            "startRowIndex": start_row,
-                            "endRowIndex": end_row,
-                            "startColumnIndex": 7,  # столбец Н, по умолчанию 7
-                            "endColumnIndex": 9  # столбец I, по умолчанию 9
-                        },
-                        "pasteType": "PASTE_VALUES",  # вставляем как значения, бэкапим статусы через 5 дней
-                        "pasteOrientation": "NORMAL"
-                    }
+        "requests": [
+            {
+                "copyPaste": {
+                    "source": {
+                        "sheetId": history_sheet_id,
+                        "startRowIndex": start_row,
+                        "endRowIndex": end_row,
+                        "startColumnIndex": 7,  # столбец Н, по умолчанию 7
+                        "endColumnIndex": 9  # столбец I, по умолчанию 9
+                    },
+                    "destination": {
+                        "sheetId": history_sheet_id,
+                        "startRowIndex": start_row,
+                        "endRowIndex": end_row,
+                        "startColumnIndex": 7,  # столбец Н, по умолчанию 7
+                        "endColumnIndex": 9  # столбец I, по умолчанию 9
+                    },
+                    "pasteType": "PASTE_VALUES",  # вставляем как значения, бэкапим статусы через 5 дней
+                    "pasteOrientation": "NORMAL"
                 }
-            ]
-        }
+            }
+        ]
+    }
     ApiGoogle(history_data).copy_paste(backup_old_statuses_h_i)
-
-    list_of_name_phone_calls = [call["name"] for call in phone_calls]
-    calls_info = []
-    data_stat_value = ApiGoogle(history_data, [f"Q4_22!A{start_row+1}:I{end_row}"]).read_data_ranges()
-    for name in list_of_name_phone_calls:
-        if name == "s7d":
-            rows = [row for row in data_stat_value["valueRanges"][0]["values"]
-                    if row[0] == "step 7" or row[0] == "decision"]
-        else:
-            rows = [row for row in data_stat_value["valueRanges"][0]["values"] if row[0] == name]
-        all_rows = len(rows)
-        statuses = len([el[0] for el in rows if len(el) > 7])
-        reason_code = len([el[0] for el in rows if len(el) > 8])
-        calls_info.append({f"{name}": {"all_rows": all_rows, "statuses": statuses, "reason_code": reason_code}})
-
-    return {"name": "statuses + reson code", "func": "backed up", "result": "true",
-            "info": {"date_for_backup": date_backup_statuses, "from_cell": f"H{start_row+1}", "to_cell": f"I{end_row}",
-                     "detail_info": calls_info}}
+    return check_backup_statuses(history_data, history_sheet_name, start_row, end_row, date_backup_statuses)
 
 
-def send_report(copying_results, list_add_statuses, start_time, end_time, my_email, password, to_emails):
+def send_report(list_copying_results, list_add_statuses, start_time, end_time, my_email, password, to_emails):
     message = ""
     message += f"Start time: {start_time}\n\n"
-    for result in copying_results:
+    for result in list_copying_results:
         if result['result'] == "true":
             text = f"The data {result['name'].upper()} was {result['func']} successfully: {result['info']}.\n\n"
         elif result['result'] == "true_2":
@@ -247,31 +268,52 @@ def send_report(copying_results, list_add_statuses, start_time, end_time, my_ema
 
 
 def main():
-    start_time = dt.datetime.utcnow()+dt.timedelta(hours=3)
-    copying_results = []
+    start_time = dt.datetime.utcnow() + dt.timedelta(hours=3)
+    list_copying_results = []
     list_add_statuses = []
-    for key in phone_calls:
+    for i in range(0, 2):
         try:
-            result_copying = copying_main_data(key["name"], key["call_list"])
-        except Exception as exc_1:
-            result_copying = {"name": key["name"], "func": "copied", "result": "false", "info": exc_1}
-        copying_results.append(result_copying)
+            result_backup_st = backup_statuses(
+                history_data=HISTORY["history_data"][i],
+                history_sheet_id=HISTORY["history_sheet_id"][i],
+                history_sheet_name=HISTORY["history_sheet_name"][i])
+        except Exception as exc_3:
+            result_backup_st = {"name": "statuses + reson code", "func": "backed up", "result": "false", "info": exc_3}
+        list_copying_results.append(result_backup_st)
 
-        if result_copying["result"] == "true":
+    for call in CALLS:
+        try:
+            result_copying = copying_main_data(name=call["name"],
+                                               call_list=call["call_list"],
+                                               history_data=HISTORY["history_data"][0],
+                                               history_sheet_id=HISTORY["history_sheet_id"][0],
+                                               history_sheet_name=HISTORY["history_sheet_name"][0])
+        except Exception as exc_1:
+            result_copying = {"name": call["name"], "func": "copied", "result": "false", "info": exc_1}
+        list_copying_results.append(result_copying)
+
+        if call["name"] == "step 2-3":
             try:
-                result_add_status = add_statuses_as_formula(result_copying)
+                result_copying_2 = copying_main_data(name=call["name"],
+                                                     call_list=call["call_list"],
+                                                     history_data=HISTORY["history_data"][1],
+                                                     history_sheet_id=HISTORY["history_sheet_id"][1],
+                                                     history_sheet_name=HISTORY["history_sheet_name"][1])
+            except Exception as exc_1:
+                result_copying_2 = {"name": call["name"], "func": "copied", "result": "false", "info": exc_1}
+            list_copying_results.append(result_copying_2)
+
+    for result in list_copying_results:
+        if result["result"] == "true" and result["func"] == "copied":
+            try:
+                result_add_status = add_statuses_as_formula(result)
             except Exception as exc_2:
-                result_add_status = [{"name": result_copying["name"], "func": "added",
+                result_add_status = [{"name": result["name"], "func": "added",
                                       "result": "false", "info": exc_2}]
             list_add_statuses.append(result_add_status)
-    try:
-        result_backup_st = backup_statuses()
-    except Exception as exc_3:
-        result_backup_st = {"name": "statuses + reson code", "func": "backed up", "result": "false", "info": exc_3}
-    copying_results.append(result_backup_st)
-    end_time = dt.datetime.utcnow()+dt.timedelta(hours=3)
+    end_time = dt.datetime.utcnow() + dt.timedelta(hours=3)
 
-    send_report(copying_results, list_add_statuses, start_time, end_time, MY_EMAIL, PASSWORD, TO_EMAILS)
+    send_report(list_copying_results, list_add_statuses, start_time, end_time, MY_EMAIL, PASSWORD, TO_EMAILS)
 
 
 if __name__ == "__main__":
